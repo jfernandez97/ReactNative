6cc99afc4039d5fc39544d28143da8eca4e9e62e
Juan Fernandez <juan.fernandez@mi.unc.edu.ar>
2021-12-09 00:02:46 -0300

    tasks and addtask

--- App.js
+++ App.js
@@ -1,59 +1,109 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  PlatformColor,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useState } from "react";
 
-import { StatusBar } from "expo-status-bar";
+import Task from "./components/Task";
 
 export default function App() {
-  const [list, setList] = useState([]);
+  const [task, setTask] = useState("");
 
-  const [textInput, setTextInput] = useState("");
+  const [taskItems, setTaskItems] = useState([]);
 
-  const onAdd = () => {
-    setList([...list, textInput]);
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task]);
+    setTask("");
+  };
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
   };
   return (
-    <>
-      <View style={styles.containerText}>
-        <Text>Hola,Coder!</Text>
-        <StatusBar style="auto" />
-      </View>
-      <View style={styles.container}>
-        <View style={styles.containerInput}>
-          <TextInput
-            placeholder="Escribe Aqui"
-            onChangeText={(text) => setTextInput(text)}
-            style={styles.input}
-          ></TextInput>
-          <Button title="Add" onPress={() => onAdd()} />
+    <View style={styles.container}>
+      {/* Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}> Tasks...</Text>
+        <View style={styles.items}>
+          {/* This is where the tasks will go */}
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item}></Task>
+              </TouchableOpacity>
+            );
+          })}
         </View>
       </View>
-    </>
+      {/* Write a task */}
+      <KeyboardAvoidingView style={styles.writeTaskWrapper}>
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task..."}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        ></TextInput>
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: "100%",
-    backgroundColor: "#fff",
+    flex: 1,
+    backgroundColor: "#E8EAED",
   },
-  containerText: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "5%",
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
   },
-  containerInput: {
-    // backgroundColor: "blue",
-    flexDirection: "row",
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
     width: "100%",
-    height: "10%",
-    marginTop: "3%",
-    justifyContent: "space-around",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
   },
   input: {
-    borderBottomColor: "grey",
-    borderBottomWidth: 5,
-    width: "70%",
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
   },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {},
 });
---
+++ components/Task.js
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import App from "../App";
+import React from "react";
+
+const Task = (props) => {
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#BBAACC",
+    borderRadius: 5,
+    opacity: 0.7,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: "#BBAACC",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
+
+export default Task;
